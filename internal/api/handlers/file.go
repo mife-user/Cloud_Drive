b24ec202b9e4d77092b6e8ada8ac642b0f99a4a0
mife <15723556393@163.com>
2026-02-05 18:49:17 +0800

    refactor(service): 将文件保存逻辑提取到service层
    将文件上传处理中的线程池和保存文件逻辑从handler层提取到service层，提高代码复用性和可维护性

--- internal/api/handlers/file.go
+++ internal/api/handlers/file.go
@@ -4,9 +4,8 @@ import (
 	"net/http"
 
 	"drive/internal/domain"
+	"drive/internal/service"
 	"drive/pkg/conf"
-	"drive/pkg/pool"
-	"drive/pkg/utils"
 
 	"github.com/gin-gonic/gin"
 )
@@ -42,23 +41,12 @@ func (h *FileHandler) UploadFile(c *gin.Context) {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "未认证用户"})
 		return
 	}
-	// 保存文件
 	var fileRecords []*domain.File
-	pool := pool.NewPool(4)
-	pool.Start()
-	for _, header := range files {
-		// 提交任务到线程池
-		pool.Submit(func() {
-			fileRecord, err := utils.SaveFile(header, userID.(uint))
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": "保存文件失败: " + err.Error()})
-				return
-			}
-			// 保存文件记录到切片
-			fileRecords = append(fileRecords, fileRecord)
-		})
+	// 保存文件
+	if err = service.SaveFiles(c, files, fileRecords, userID.(uint)); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "保存文件失败: " + err.Error()})
+		return
 	}
-	pool.Stop()
 
 	// 保存文件记录到数据库
 	if err := h.fileRepo.UploadFile(c, fileRecords); err != nil {
---
+++ internal/service/file_sv.go
@@ -0,0 +1,32 @@
+package service
+
+import (
+	"drive/internal/domain"
+	"drive/pkg/logger"
+	"drive/pkg/pool"
+	"drive/pkg/utils"
+	"mime/multipart"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+func SaveFiles(c *gin.Context, files []*multipart.FileHeader, fileRecords []*domain.File, userID uint) error {
+	// 保存文件
+	pool := pool.NewPool(4)
+	pool.Start()
+	for _, header := range files {
+		// 提交任务到线程池
+		pool.Submit(func() {
+			fileRecord, err := utils.SaveFile(header, userID)
+			if err != nil {
+				logger.Error("保存文件失败: %v", zap.Error(err))
+				return
+			}
+			// 保存文件记录到切片
+			fileRecords = append(fileRecords, fileRecord)
+		})
+	}
+	pool.Stop()
+	return nil
+}
